89340b080e93b73a09305a89dd87f60b879c215e
Victor Chp <qatester906@gmail.com>
2023-04-13 13:03:07 +0300

    Fix print

--- test_homework.py
+++ test_homework.py
@@ -11,6 +11,7 @@ def test_greeting():
     age = 25
     # TODO Сформируйте нужную строку
     output = f"Привет, {name}! Тебе {age} лет."
+    print(f"\n{output}")
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
@@ -40,10 +41,12 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = r ** 2 * math.pi
+    print(f"\nПлощадь круга {area}")
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = 2 * math.pi * r
+    print(f"Длина окружности {length}")
     assert length == 144.51326206513048
 
 
@@ -97,6 +100,7 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = dict(zip(first, second))
+    print(f"\nСловарь {d}")
 
     assert isinstance(d, dict)
     assert len(d) == 5
